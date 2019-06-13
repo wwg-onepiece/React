eb3652a05448442e595a1167588205a849e2e91b
wwg-onepiece <1298516393@qq.com>
2019-06-13 11:50:27 +0800

    first commit

---
+++ src/components/App.2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// 组件就是一个函数，返回HTML代码，记得带()
+// const App = function () {
+//   return (
+//     <div>hello React - function</div>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <div>hello React - function</div>
+//   )
+// }
+
+const App = () => (<div>hello React - function</div>)
+
+// 一定不要忘记暴露组件
+export default App;
---
+++ src/components/App.4.js
@@ -0,0 +1,24 @@
+import React,{ Component } from 'react'; // 类组件的另一种引用方式
+class App extends Component { // 继承时不用写React
+  constructor (props) {
+    super(props);
+    this.state = {
+      list: ['a', 's', 'd', 'f']
+    }
+  }
+  render () {
+    return (
+      <ul>
+        {
+          this.state.list.map((item, index) => {
+            return (<li key = { index }>
+              { item }
+            </li>)
+          })
+        }
+      </ul>
+    )
+  }
+}
+
+export default App;
