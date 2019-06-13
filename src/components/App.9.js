0c9f0dca9d418a955c1c06c9d6487d21ec687256
wwg-onepiece <1298516393@qq.com>
2019-06-13 17:48:34 +0800

    测试

---
+++ src/components/App.9.js
@@ -0,0 +1,36 @@
+import React,{ Component } from 'react';
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      list: [
+        {
+          brand: 'yys',
+          data: ['大天狗', '茨木童子', '彼岸花']
+        },
+        {
+          brand: 'LOL',
+          data: ['EZ', '凯南']
+        }
+      ]
+    }
+  }
+  render () {
+    let arr = [];
+    this.state.list.map((item, index) => {
+      let ar = [];
+      item.data.map((itm, idx) => {
+        ar.push(<li key = { idx }>{ itm }</li>)
+      })
+      let oli = <li key = { index }>{ item.brand }<ul>{ ar }</ul></li>;
+      arr.push(oli)
+    })
+    return (
+      <ul>
+        { arr }
+      </ul>
+    )
+  }
+}
+
+export default App;
