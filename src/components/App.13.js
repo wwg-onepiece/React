eb5d7e1ffb703eb11deb307ce1d0ee1bf1312eb0
wwg-onepiece <1298516393@qq.com>
2019-06-13 18:00:47 +0800

    事件中传参-推荐使用

---
+++ src/components/App.13.js
@@ -0,0 +1,20 @@
+// 事件中传参，推荐使用
+import React,{ Component } from 'react';
+class App extends Component {
+  constructor (props) {
+    super(props);
+    console.log('1', this)
+  }
+  btnClickFn (id) {
+    console.log(id, this);
+  }
+  render () {
+    return (
+      <div>
+        <button onClick = { this.btnClickFn.bind(this, 22) }>react按钮</button>
+      </div>
+    )
+  }
+}
+
+export default App;
