87f765fd612b32ec5af755efb68e4c59ee500902
wwg-onepiece <1298516393@qq.com>
2019-06-13 17:58:51 +0800

    事件中传参-推荐使用

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,18 @@
+// 事件中传参，推荐使用
 import React,{ Component } from 'react';
 class App extends Component {
   constructor (props) {
     super(props);
-    this.state = {
-      list: [
-        {
-          brand: 'yys',
-          data: ['大天狗', '茨木童子', '彼岸花', '花鸟卷']
-        },
-        {
-          brand: 'LOL',
-          data: ['EZ', '洛', '女警', '凯南']
-        }
-      ]
-    }
+    console.log('1', this)
+  }
+  btnClickFn (id) {
+    console.log(id, this);
   }
   render () {
-    let arr = [];
-    this.state.list.map((item, index) => {
-      let ar = [];
-      item.data.map((itm, idx) => {
-        ar.push(<li key = { idx }>{ itm }</li>)
-      })
-      let oli = <li key = { index }>{ item.brand }<ul>{ ar }</ul></li>;
-      arr.push(oli)
-    })
     return (
-      <ul>
-        { arr }
-      </ul>
+      <div>
+        <button onClick = { this.btnClickFn.bind(this, 22) }>react按钮</button>
+      </div>
     )
   }
 }
